5c12db7e92877d219e3b49105bd09bdf26d28d15
Jeong MinGyu <tomorrow9913@gmail.com>
2024-07-19 00:06:21 +0900

    Feat: Change Something

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -1,16 +1,22 @@
-#include <stddef.h> // for size_t
-#include <stdint.h> // for uint8
-#include <stdlib.h> // for printf    
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>    
 #include <iostream>
-
+#include <memory>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define READ_SIZE 4
+
 using namespace std;
 
-// 파일에서 숫자을 읽기 위해서는 fopen, fread 함수 등을 사용한다. fopen하고 나서 fclose 함수도 호출해 주도록 한다. 관련 함수들의 사용 방법은 검색을 통하여 익힌다.
-// 정수 덧셈에서 발생하는 overflow는 무시한다.
-// 파일이 제대로 열리지 않는 경우, 파일의 크기가 4바이트보다 많거나 작아서 제대로 읽지 못했다고 판단되는 경우 등에 대해서 에러 처리를 할 수 있도록 한다.
+struct FileDeleter {
+    void operator()(FILE* file) const {
+        if (file) fclose(file);
+    }
+};
+
+using FilePtr = unique_ptr<FILE, FileDeleter>;
 
 uint32_t GetFileSize(FILE* file){
     fseek(file, 0, SEEK_END);
@@ -19,24 +25,29 @@ uint32_t GetFileSize(FILE* file){
     return size;
 }
 
-FILE* fopen_(const char* filename, const char* mode){
+FilePtr fopen_(const char* filename, const char* mode){
     FILE* file = fopen(filename, mode);
-    if(file == NULL){
-        printf("Can not open File: %s\n", filename);
+    if(!file){
+        fprintf(stderr, "Can not open File: %s\n", filename);
         exit(-1);
     }
+ 
     uint32_t filesize = GetFileSize(file);
-    if(filesize != 4){
-        printf("File size is not 4 byte: %s(FileSize: %d)\n", filename, filesize);
+    if(filesize != READ_SIZE){
+        fprintf(stderr, "File size is not %d byte: %s(FileSize: %d)\n", READ_SIZE, filename, filesize);
         exit(-1);
     }
 
-    return file;
+    return FilePtr(file) ;
 }
 
 uint32_t getFileData(FILE* file, size_t size) {
     uint32_t num;
-    fread(&num, sizeof(uint32_t), 1, file);
+    if(fread(&num, sizeof(int8_t), READ_SIZE, file) < READ_SIZE) {
+        fprintf(stderr, "Error reading file\n");
+        exit(-1);
+    }
+
     num = ntohl(num);
     return num;
 }
@@ -44,19 +55,16 @@ uint32_t getFileData(FILE* file, size_t size) {
 int main(int argc, char **argv){
     if (argc !=  3){
         printf("Usage: add-nbo <file1> <file2>\n");
-        return -1;
+        exit(-1);
     }
     
-    FILE* file1 = fopen_(argv[1], "rb");
-    FILE* file2 = fopen_(argv[2], "rb");
+    FilePtr file1 = fopen_(argv[1], "rb");
+    FilePtr file2 = fopen_(argv[2], "rb");
     
-    uint32_t num1= getFileData(file1, 4);
-    uint32_t num2= getFileData(file2, 4);
+    uint32_t num1 = getFileData(file1.get(), READ_SIZE);
+    uint32_t num2 = getFileData(file2.get(), READ_SIZE);
 
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", num1, num1, num2, num2, num1 + num2, num1 + num2);
     
-    fclose(file1);
-    fclose(file2);
     return 0;
-
-}
+}   
